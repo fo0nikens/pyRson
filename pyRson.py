6a4e04c871e4d5a02a12229906640ed3eee4b8c9
kernoeb <kernoeb@protonmail.com>
2019-04-24 10:56:27 +0200

    First commit 🎉
    And it works

---
+++ pyRson.py
@@ -0,0 +1,53 @@
+####################### 
+#  PERSON GENERATOR   #
+#    by @kernoeb	  #
+# github.com/kernoeb  #
+# twitter.com/kernoeb #
+# #####################
+
+import urllib.request, urllib.parse, urllib.error
+import http.client
+import requests, json
+
+# Générer une image depuis le site thispersondoesnotexist.com
+headers_tpdne = {
+    'user-agent': 'ok',
+}
+
+response_tpdne = requests.get('https://thispersondoesnotexist.com/image', headers=headers_tpdne)
+open('yes.jpeg', 'wb').write(response_tpdne.content)
+
+
+# FaceAPI Azure Microsoft
+# Veuillez utiliser votre "subscription key"
+faceapi_headers = {
+    'Content-type': 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key': 'SUBSCRIPTION KEY HERE',
+}
+
+# Paramètres
+faceapi_params = urllib.parse.urlencode({
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age,gender,glasses',
+})
+
+# Point de terminaison (à modifier si différent)
+# Voir la vue d'ensemble de votre ressource Microsoft Azure
+conn = http.client.HTTPSConnection('francecentral.api.cognitive.microsoft.com')
+
+# Ouvrir l'image
+f = open("yes.jpeg", "rb")
+faceapi_body = f.read()
+f.close()
+
+# Requête POST et envoi de l'image
+conn.request("POST", "/face/v1.0/detect?%s" % faceapi_params, faceapi_body, faceapi_headers)
+response = conn.getresponse()
+data = response.read()[1:-1]
+
+# Lecture en json pour simplifier
+j = json.loads(data)
+
+# Affichage du sexe de la personne (male / female)
+print(j['faceAttributes']['gender'])
